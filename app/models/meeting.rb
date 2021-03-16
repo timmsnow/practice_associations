9ebfd8c66102e87e9a35c6eb9c2d6187fa4c214f
TIMM SNOW <two.tone.snow@gmail.com>
2021-03-16 08:54:30 -0400

    adds validation preventing creation of meeting based on time

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,4 +1,12 @@
 class Meeting < ApplicationRecord
   has_many :meeting_speakers
   has_many :speakers, through: :meeting_speakers
+
+  validate :meeting_date_cannot_be_in_the_past
+
+  def meeting_date_cannot_be_in_the_past
+    if time < Time.now
+      errors.add(:time, "can't be in the past")
+    end
+  end
 end
